3ea4ec46b132dead9e33ddb03b5317fca494a151
0xd34df00d <0xd34df00d@gmail.com>
2020-02-29 18:09:14 -0500

    Use the SSE4.2 implementation + userspace counting only

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,16 @@
 module Main where
 
+import Control.Exception
 import System.Environment
 import System.IO.Posix.MMap
+import System.TimeIt
 
-import Data.ByteString.CountByte
+import Data.ByteString.CountByte.Asm.SSE42
 
 main :: IO ()
 main = do
   [path] <- getArgs
   contents <- unsafeMMapFile path
-  print $ countByte contents 10
+  (time, counts) <- timeItUserspaceT $ evaluate $ sum $ map (`countChars` contents) (replicate 10 32)
+  print $ counts `div` 10
+  print $ time * 1000 / 10
